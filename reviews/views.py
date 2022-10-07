f0f37db2773cf6bd4d07f098285224d96f7addb2
JeongJinGan <rksaudwls123@naver.com>
2022-10-07 20:27:47 +0900

    add search

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+
 from .forms import ReviewForm
 from .models import Review
 
@@ -55,3 +56,26 @@ def delete(request, pk):
     reviews.delete()
 
     return redirect("reviews:index")
+
+
+def search(request):
+    search = request.GET.get("search")
+    reviews = Review.objects.filter(movie_name__contains=search)
+
+    if len(search) == 0:
+        reviews = []
+        text = "검색어를 입력해주세요"
+
+    elif len(reviews) == 0:
+        text = "검색 결과가 없습니다"
+
+    else:
+        print(reviews)
+        text = ""
+
+    context = {
+        "reviews": reviews,
+        "text": text,
+    }
+
+    return render(request, "reviews/search.html", context)
